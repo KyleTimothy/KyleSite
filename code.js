ff4b7c6ab3229f670e6581604e1208b68e7c67c9
KyleTimothy <15368505+KyleTimothy@users.noreply.github.com>
2020-12-05 17:48:41 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,237 @@
+
+///////////////////////////////////////////////////////////////////////////////////////Navy Marine ribbon
+let NavyMarine = document.getElementById("navyAndMarineRibbon");
+
+NavyMarine.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/7/76/Navy_and_Marine_Corps_Medal_ribbon.svg"
+
+let src = document.getElementById("navyAndMarineRibbon")
+
+src.appendChild(img)
+
+})
+
+   NavyMarine.addEventListener("mouseover", showMe =>{
+ document.getElementById("navyAndMarineRibbon").innerHTML="Navy Marine Ribbon"
+
+}) 
+
+
+NavyMarine.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/7/76/Navy_and_Marine_Corps_Medal_ribbon.svg"
+
+let src = document.getElementById("navyAndMarineRibbon")
+
+src.appendChild(img)
+
+NavyMarine.addEventListener("mouseout", showMe =>{
+ document.getElementById("navyAndMarineRibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////National Defense
+let NationalDefense = document.getElementById("nationalDefenseRibbon");
+
+NationalDefense.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/National_Defense_Service_Medal_ribbon.svg/1280px-National_Defense_Service_Medal_ribbon.svg.png"
+
+let src = document.getElementById("nationalDefenseRibbon")
+
+src.appendChild(img)
+
+})
+
+NationalDefense.addEventListener("mouseover", showMe =>{
+ document.getElementById("nationalDefenseRibbon").innerHTML="National Defense Ribbon"
+
+}) 
+
+
+NationalDefense.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/National_Defense_Service_Medal_ribbon.svg/1280px-National_Defense_Service_Medal_ribbon.svg.png"
+
+let src = document.getElementById("nationalDefenseRibbon")
+
+src.appendChild(img)
+
+NationalDefense.addEventListener("mouseout", showMe =>{
+ document.getElementById("nationalDefenseRibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
+/////////////////////////////////////////////////
+
+let NavyMarineRib = document.getElementById("navyMarineRibbon");
+
+NavyMarineRib.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/b/be/Navy_and_Marine_Corps_Overseas_Service_Ribbon.svg"
+
+let src = document.getElementById("navyMarineRibbon")
+
+src.appendChild(img)
+
+})
+
+NavyMarineRib.addEventListener("mouseover", showMe =>{
+ document.getElementById("navyMarineRibbon").innerHTML="Navy Marine Ribbon"
+
+}) 
+
+
+NavyMarineRib.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/b/be/Navy_and_Marine_Corps_Overseas_Service_Ribbon.svg"
+
+let src = document.getElementById("navyMarineRibbon")
+
+src.appendChild(img)
+
+NavyMarineRib.addEventListener("mouseout", showMe =>{
+ document.getElementById("navyMarineRibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
+/////////////////////////////////////////////////////////////////
+console.log("Korea Defense section")
+
+let KoreaDefense = document.getElementById("koreaDefenseRibbon");
+
+KoreaDefense.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/1/17/Korea_Defense_Service_Medal_ribbon.svg"
+
+let src = document.getElementById("koreaDefenseRibbon")
+
+src.appendChild(img)
+
+})
+
+KoreaDefense.addEventListener("mouseover", showMe =>{
+ document.getElementById("koreaDefenseRibbon").innerHTML="Korea Defense Ribbon"
+
+}) 
+
+
+KoreaDefense.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/1/17/Korea_Defense_Service_Medal_ribbon.svg"
+
+let src = document.getElementById("koreaDefenseRibbon")
+
+src.appendChild(img)
+
+KoreaDefense.addEventListener("mouseout", showMe =>{
+ document.getElementById("koreaDefenseRibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
+
+//////////////////////////////////////////////////////////////////////////////
+console.log()
+let NavyConduct = document.getElementById("navyConductRibbon");
+
+NavyConduct.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/a/ac/U.S._Navy_Good_Conduct_Medal_ribbon.svg"
+
+let src = document.getElementById("navyConductRibbon")
+
+src.appendChild(img)
+
+})
+
+NavyConduct.addEventListener("mouseover", showMe =>{
+ document.getElementById("navyConductRibbon").innerHTML="Navy Conduct Ribbon"
+
+}) 
+
+
+NavyConduct.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/a/ac/U.S._Navy_Good_Conduct_Medal_ribbon.svg"
+
+let src = document.getElementById("navyConductRibbon")
+
+src.appendChild(img)
+
+NavyConduct.addEventListener("mouseout", showMe =>{
+ document.getElementById("navyConductRibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
+
+//////////////////////////////////////////////////////////////////////////////
+console.log()
+let BattleE = document.getElementById("battleERibbon");
+
+BattleE.addEventListener("mouseover", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Battle-e-ribbon.png/340px-Battle-e-ribbon.png"
+
+let src = document.getElementById("battleERibbon")
+
+src.appendChild(img)
+
+})
+
+BattleE.addEventListener("mouseover", showMe =>{
+ document.getElementById("battleERibbon").innerHTML="Battle 'E' Ribbon"
+
+}) 
+
+
+BattleE.addEventListener("mouseout", showMePic =>{
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Battle-e-ribbon.png/340px-Battle-e-ribbon.png"
+
+let src = document.getElementById("battleERibbon")
+
+src.appendChild(img)
+
+BattleE.addEventListener("mouseout", showMe =>{
+ document.getElementById("battleERibbon").innerHTML=""
+
+
+ src.appendChild(img)
+
+}) 
+
+})
